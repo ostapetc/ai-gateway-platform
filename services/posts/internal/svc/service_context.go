e5ea645543c6f329f995597a6c6a9aef6f408b99
Artem Ostapets <aostapets@aflt-systems.ru>
2026-05-15 16:07:20 +0300

    posts service update

--- services/posts/internal/svc/service_context.go
+++ services/posts/internal/svc/service_context.go
@@ -15,12 +15,6 @@ type ServiceContext struct {
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	postStore := store.NewPostStore()
-	postStore.Add(1, "Title for the first Post", "Content for the first Post.")
-	postStore.Add(2, "Title for the second Post", "Content for the second Post.")
-	postStore.Add(3, "Title for the third Post", "Content for the third Post.")
-	postStore.Add(4, "Title for the fourth Post", "Content for the fourth Post.")
-	postStore.Add(5, "Title for the fifth Post", "Content for the fifth Post.")
-	postStore.Add(6, "Title for the sixth Post", "Content for the sixth Post.")
 
 	return &ServiceContext{
 		Config:    c,
