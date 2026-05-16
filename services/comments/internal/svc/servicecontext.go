03e3bbb4cbf502a0ad314e4592d5a7d87dbbb69d
Artem Ostapets <aostapets@aflt-systems.ru>
2026-05-16 19:49:52 +0300

    deleted mock comments

--- services/comments/internal/svc/servicecontext.go
+++ services/comments/internal/svc/servicecontext.go
@@ -12,11 +12,6 @@ type ServiceContext struct {
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	commentsStore := store.NewCommentStore()
-	commentsStore.Add(1, 1, "post 1 comment 1")
-	commentsStore.Add(2, 1, "post 1 comment 2")
-	commentsStore.Add(3, 1, "post 1 comment 3")
-	commentsStore.Add(4, 2, "post 2 comment 1")
-	commentsStore.Add(5, 2, "post 2 comment 2")
 
 	return &ServiceContext{
 		Config:       c,
